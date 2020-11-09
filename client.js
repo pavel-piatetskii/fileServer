982cb5ce6ee10fff12c18686d5b41699f2e91e79
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-09 06:22:13 +0000

    Server can send any type of file

--- client.js
+++ client.js
@@ -1,44 +1,22 @@
 const net = require('net');
 const fs = require('fs');
-const file = process.argv[2];
-
-// ---- Actual function to save a file ---
-const saveFileYes = function(data, path) {
-  fs.writeFile(`./clientFiles/${path}`, data, (err) => {
-    if (err) throw err;
-    console.log(`Downloaded and saved ` +
-                `${data.length} bytes to ${path}.`);
-    
-    process.exit();
-  })
-};
-
+const [ , , host, port, file] = process.argv;
 
 const conn = net.createConnection({ 
-  host: '10.0.2.15', // change to IP address of computer or ngrok host if tunneling
-  port: 3000 // or change to the ngrok port if tunneling
+  host, // change to IP address of computer or ngrok host if tunneling
+  port // or change to the ngrok port if tunneling
 });
 
-conn.setEncoding('utf8'); // interpret data as text
-
+let fileToSave =  fs.createWriteStream(`./clientFiles/${file}`)
 let transfer = false;
 conn.on('data', (data) => {
-  
-   if (transfer) {
-     saveFileYes(data, file)
-   } else {
-     console.log('Server says: ', data);
-   }
-   
-   if (data === `File ${file} has been found!`) {
-     transfer = true;
-   }
-   
-   if (data === `Cannot find a file ${file}`) {
-     process.exit();
-   }
+  fileToSave.write(data);
 });
 
 conn.on('connect', () => {
   conn.write(file);
+});
+
+conn.on('close', () => {
+  console.log(`Done! File ${file} received.`);
 });
--- server.js
+++ server.js
@@ -1,33 +1,20 @@
 const net = require('net');
 const fs = require('fs');
 
-/**
- * Load a file from filesystem
- */
-const loadFile = function(path, callback) {
-  fs.readFile(`./serverFiles/${path}`, 'utf8', (error, data) => {
-    (error) ? callback(error) : callback(data);
-  });
-};
-
 const server = net.createServer();
 
 server.on('connection', (client) => {
   console.log('New client connected!');
-  // client.write('Hello there!');
 
-  client.setEncoding('utf8'); // interpret data as text
   client.on('data', (data) => {
-    //console.log(data)
-
+  console.log(client)
     if (fs.existsSync(`./serverFiles/${data}`)) {
-      client.write(`File ${data} has been found!`)
-      loadFile(data, (file) => {
-        client.write(file);
-      });
+      console.log(`Sending file ${data}`)
+      fs.createReadStream(`./serverFiles/${data}`).pipe(client);
 
     } else {
-      client.write(`Cannot find a file ${data}`)
+      console.log(`File ${data} not found. Sending file-not-found`)
+      fs.createReadStream(`./serverFiles/file-not-found`).pipe(client);
     }
   });
 });
